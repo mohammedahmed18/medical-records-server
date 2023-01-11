edd0289fd73210adde37805e77356c067feac4f8
mohammedahmed18 <mohammed18200118@gmail.com>
2023-01-11 15:51:13 +0200

    fix a typo

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,6 +1,6 @@
 import { commons } from './common';
 export const error_msgs = {
-  UNAUTHORIZED_LOGIN: 'Invalid email or password',
+  UNAUTHORIZED_LOGIN: 'Invalid nationalId or password',
   NATIONALID_LENGTH_ERROR: `National id must be exactly ${commons.nationalId_chars} characters`,
   ACCOUNT_ALREADY_EXISTS: (field : string = "resource") => `This ${field} already exists`,
 };
