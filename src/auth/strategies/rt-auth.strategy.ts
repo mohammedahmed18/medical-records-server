731fea8dac0168dd701e9c4c2a0093702fa8cd74
mohammedahmed18 <mohammed18200118@gmail.com>
2023-01-18 16:06:45 +0200

    fix long imports

--- src/auth/strategies/rt-auth.strategy.ts
+++ src/auth/strategies/rt-auth.strategy.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { JwtPayload } from '../../common/types';
+import { JwtPayload } from 'src/common/types';
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
