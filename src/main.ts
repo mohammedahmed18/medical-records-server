5a9b400d178558e06eb4890d13dc5f02279bd57f
mohammedahmed18 <mohammed18200118@gmail.com>
2023-02-15 20:32:05 +0200

    log a message when the server is running , comment the redis server for now

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ErrorInterceptor } from './common/interceptors';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.enableCors()
   const port = process.env.PORT || 3000;
 
-  await app.listen(port);
+  await app.listen(port , () => {
+    Logger.log(`application is running on port ${port}`)
+  });
 }
 bootstrap();
