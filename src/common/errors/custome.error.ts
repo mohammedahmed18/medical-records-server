46d43a502e3ee34c6924eb39ef05e24b8237e3df
mohammedahmed18 <mohammed18200118@gmail.com>
2023-02-27 23:06:12 +0200

    wrong commit

--- src/common/errors/custome.error.ts
+++ src/common/errors/custome.error.ts
@@ -5,7 +5,7 @@ export class CustomError extends HttpException {
   constructor(options: CustomErrorOptions) {
     options.statusCode = options.statusCode || 500;
     options['message'] = options.msg;
-    delete options.msg;
+    delete options.msgf;
     super(options, options.statusCode);
   }
 }
