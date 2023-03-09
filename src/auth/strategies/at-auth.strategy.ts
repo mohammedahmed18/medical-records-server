9a4b89f392b22e430e2fc30dfc0dec4d59d00924
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-09 09:14:30 +0200

    get the token from cookies if it's not in auth header

--- src/auth/strategies/at-auth.strategy.ts
+++ src/auth/strategies/at-auth.strategy.ts
@@ -8,7 +8,18 @@ import { JwtPayload } from 'src/common/types';
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ExtractJwt.fromExtractors([
+          (req) => {
+            let token = null;
+            if (req && req.cookies) {
+              token = req.cookies['token'];
+            }
+            return token;
+          },
+        ]),
+      ]),
       secretOrKey: config.get<string>('JWT_SECRET'),
     });
   }
