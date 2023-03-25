698a66a408c149d42af488ec412b536a0e2f6bf6
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-25 06:00:16 +0200

    fix: import

--- src/common/interceptors/error.interceptor.ts
+++ src/common/interceptors/error.interceptor.ts
@@ -1,4 +1,4 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import {
   CallHandler,
   ExecutionContext,
