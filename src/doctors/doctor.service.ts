3929273964a4c41ed7061122695342a216d80340
mohammedahmed18 <mohammed18200118@gmail.com>
2023-05-22 10:54:22 +0300

    fix: incase sensitive doctor search , update Dockerfile

--- src/doctors/doctor.service.ts
+++ src/doctors/doctor.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CustomError } from 'src/common/errors';
 import { SCAN_YOUR_SELF_ERR_CODE } from 'src/constants';
 import { PrismaService } from 'src/database/prisma.service';
@@ -9,15 +13,18 @@ import { resizeCloudinaryImage } from 'src/utils/resizeCloudinaryImage';
 
 @Injectable()
 export class DoctorService {
-  constructor(private readonly userService: UsersService,
-              private readonly prisma : PrismaService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly prisma: PrismaService,
+  ) {}
 
-  async scanQrCode(patientNationalId: string , doctorNationalId : string) {
-    if(patientNationalId === doctorNationalId) throw new CustomError({
-      errorCode : SCAN_YOUR_SELF_ERR_CODE,
-      message : "you can't scan your qr code",
-      statusCode : 400
-    })
+  async scanQrCode(patientNationalId: string, doctorNationalId: string) {
+    if (patientNationalId === doctorNationalId)
+      throw new CustomError({
+        errorCode: SCAN_YOUR_SELF_ERR_CODE,
+        message: "you can't scan your qr code",
+        statusCode: 400,
+      });
     return await this.userService.findByNationalId(patientNationalId, {
       id: true,
       image_src: true,
@@ -25,45 +32,58 @@ export class DoctorService {
     });
   }
 
-  async getAllDoctors(options : getDoctorsOptions){
+  async getAllDoctors(options: getDoctorsOptions) {
+    const { medicalSpecialization, cursor, perPage, search } = options;
 
-    const { medicalSpecialization , cursor , perPage , search} = options
+    if (
+      medicalSpecialization &&
+      !Object.keys(MedicalSpecialization).includes(medicalSpecialization)
+    )
+      throw new BadRequestException('medical specialization not allowed');
 
-    if(medicalSpecialization && ! Object.keys(MedicalSpecialization).includes(medicalSpecialization) )
-      throw new BadRequestException("medical specialization not allowed")
-
-    let curosrCraiteria = undefined
-    if(cursor) curosrCraiteria = {
-        id : cursor
-    }
+    let curosrCraiteria = undefined;
+    if (cursor)
+      curosrCraiteria = {
+        id: cursor,
+      };
     const doctors = await this.prisma.user.findMany({
-      where : {NOT : {medicalSpecialization : null} , medicalSpecialization : options.medicalSpecialization , name : {contains : search}},
+      where: {
+        NOT: { medicalSpecialization: null },
+        medicalSpecialization: options.medicalSpecialization,
+        name: { contains: search, mode: 'insensitive' },
+      },
       take: perPage ? parseInt(perPage) : undefined,
-      skip : cursor ? 1 : undefined,
-      cursor : curosrCraiteria,
-      select : {
-        id : true,
-        name : true,
-        medicalSpecialization : true,
-        image_src : true
-      }
-    })
+      skip: cursor ? 1 : undefined,
+      cursor: curosrCraiteria,
+      select: {
+        id: true,
+        name: true,
+        medicalSpecialization: true,
+        image_src: true,
+      },
+    });
 
-    return doctors.map(doctor => ({...doctor , image_src : resizeCloudinaryImage(doctor.image_src , {square : true , size : 400})}))
+    return doctors.map((doctor) => ({
+      ...doctor,
+      image_src: resizeCloudinaryImage(doctor.image_src, {
+        square: true,
+        size: 400,
+      }),
+    }));
   }
 
-  async getDoctor(doctorId: string){
+  async getDoctor(doctorId: string) {
     //
-    const doctor = this.userService.findById(doctorId,{
+    const doctor = this.userService.findById(doctorId, {
       name: true,
       image_src: true,
       medicalSpecialization: true,
       email: true,
-      _count: {select : {writtenMedicalRecors : true}}
-    })
+      _count: { select: { writtenMedicalRecors: true } },
+    });
 
-    if(!doctor) throw new NotFoundException("No user found")
+    if (!doctor) throw new NotFoundException('No user found');
 
-    return doctor
+    return doctor;
   }
 }
