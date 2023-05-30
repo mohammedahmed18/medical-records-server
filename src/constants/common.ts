1231a440bca41255f9b5e5e14f37795fd4e70501
mohammedahmed18 <mohammed18200118@gmail.com>
2023-05-30 09:08:17 +0300

    add staging url to cors whitelist

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -9,6 +9,10 @@ export const MAX_PROFILE_IMAGE_SIZE_MB = 1; // 1MB
 export const CLIENT_URL = isProd
   ? 'https://medical-records-web.vercel.app'
   : 'http://localhost:3001';
-export const NETLIFY_URL = "https://medical-records1.netlify.app"
-export const SERVER_URL = isProd ? "https://medical-records-server1.onrender.com" : "http://localhost:3000"
-export const CLIENT_WHITELIST = [CLIENT_URL , NETLIFY_URL]
+export const NETLIFY_URL = 'https://medical-records1.netlify.app';
+export const SERVER_URL = isProd
+  ? 'https://medical-records-server1.onrender.com'
+  : 'http://localhost:3000';
+export const STAGING_URL =
+  'https://medical-records-web-git-staging-mohammedahmed18.vercel.app';
+export const CLIENT_WHITELIST = [CLIENT_URL, NETLIFY_URL, STAGING_URL];
